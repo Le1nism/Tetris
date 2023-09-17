54a2342bffdd66dece812c4314c590bbb22a3069
Appetible <cristian.capiferri@gmail.com>
2023-09-17 20:53:15 +0200

    Added play area

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,19 +5,24 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class GamePanel extends JPanel implements Runnable {
     
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
     final int FPS = 60;
+
     Thread gameThread;
+    PlayManager pm;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
+
+        pm = new PlayManager();
     }
 
     public void launchGame() {
@@ -52,11 +57,14 @@ public class GamePanel extends JPanel implements Runnable {
 
     private void update() {
 
-
+        pm.update();
     }
 
     public void paintComponent(Graphics g) {
 
         super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        pm.draw(g2);
     }
 }
---
+++ src/main/PlayManager.java
@@ -0,0 +1,44 @@
+package src.main;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+public class PlayManager {
+    
+    final int WIDTH = 360;
+    final int HEIGHT = 600;
+    public static int left_x;
+    public static int right_x;
+    public static int top_y;
+    public static int bottom_y;
+
+    public PlayManager() {
+
+        left_x = (GamePanel.WIDTH/2) - (WIDTH/2);
+        right_x = left_x + WIDTH;
+        top_y = 50;
+        bottom_y = top_y + HEIGHT;
+    }
+
+    public void update() {
+
+
+    }
+
+    public void draw(Graphics2D g2) {
+
+        g2.setColor(Color.white);
+        g2.setStroke(new BasicStroke(4f));
+        g2.drawRect(left_x-4, top_y-4, WIDTH+8, HEIGHT+8);
+
+        int x = right_x + 100;
+        int y = bottom_y - 200;
+        g2.drawRect(x, y, 200, 200);
+        g2.setFont(new Font("Arial", Font.PLAIN, 30));
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.drawString("NEXT", x+60, y+60);
+    }
+}
