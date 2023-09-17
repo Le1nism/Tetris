704803ea060f135402b37e3d9ec67d5ca4c49802
Appetible <cristian.capiferri@gmail.com>
2023-09-17 20:06:27 +0200

    Added game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,16 +4,59 @@ import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
+    }
+
+    public void launchGame() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    private void update() {
+
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
     }
 }
